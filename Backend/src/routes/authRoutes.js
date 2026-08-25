df7bc9b4fa149fd6cf1fd25c7bc36c2749dd35af
acharyanisha1234 <acharyanisha2063@gmail.com>
2026-08-25 18:11:37 +0545

    feat(backend): add auth routes

---
+++ Backend/src/routes/authRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { register, login, getMe } from '../controllers/authController.js';
+import { protect } from '../middleware/authMiddleware.js';
+import { body } from 'express-validator';
+const router = express.Router();
+
+router.post('/register', [
+  body('name').notEmpty(),
+  body('email').isEmail(),
+  body('password').isLength({ min: 6 }),
+], register);
+router.post('/login', login);
+router.get('/me', protect, getMe);
+
+export default router;
